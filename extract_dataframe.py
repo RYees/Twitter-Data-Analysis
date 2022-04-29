9dac073b4e94f9ab8d8f44fcb71a0daa254da945
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-04-29 20:32:09 +0300

    data modified in extract

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -1,8 +1,7 @@
 import json
+from bleach import clean
 import pandas as pd
-import tweepy
 from textblob import TextBlob
-from wordcloud imort WordCloud
 import numpy as np
 import re
 import matplotlib.pyplot as plt
@@ -43,91 +42,141 @@ class TweetDfExtractor:
     dataframe
     """
 
-    def __init__(self, tweets_list):
+    def __init__(self, tweets):
 
-        self.tweets_list = tweets_list
+        self.tweets = tweets
 
     # an example function
-    def find_statuses_count(self) -> list:
-        statuses_count = [x.get("statuses_count") for x in tweet_list["user"]]
-        for statuses_count in statuses_count:
-            return statuses_count
-        
     def find_created_time(self) -> list:
-        created_at = [x.get("created_at") for x in tweet_list["user"]]
-        for created_at in created_at:
-            return created_at
-    
-    def find_full_text(self) -> list:
-        for i in self.tweets_list["text"]:
-#             print(i)
-            return i
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        df['date'] = np.array([tweet.get('created_at') for tweet in self.tweets])
+        # print(df['date'].head(3))
+        return df['date']
     
     def find_source(self) -> list:
-        return self.tweets_list["source"]
+        df = pd.DataFrame(data=[tweet.get("source") for tweet in self.tweets], columns=['source'])
+        # print(df)
+        return df['source']
 
-    def find_sentiments(self, text) -> list:
+    def find_full_text(self) -> list:
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        return df['Tweets']
 
-        return self.tweets_list[['polarity', 'subjectivity']]
+    def clean_tweet(self):
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        cleanTweet = []
+        for tweet in df['Tweets']:
+            clean = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+            cleanTweet.append(clean)
+        # print(cleanTweet[0:5]) 
+        return cleanTweet
+
+    def find_statuses_count(self) -> list:
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        df['user'] = np.array([tweet.get('user') for tweet in self.tweets])
+        df['statuses_count'] = np.array([hash.get('statuses_count') for hash in df['user']])
+        # print(df['statuses_count'].head(3))
+        return df['statuses_count']
+
+    def find_sentiments(self):
+        analysis = self.clean_tweet()
+        # print(analysis)
+        # df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        # df['sentiments'] = np.array([x for x in df['Tweets']])
+        for analysis in analysis:
+            polarity = str(TextBlob(analysis).sentiment.polarity)
+            subjectivity = str(TextBlob(analysis).sentiment.subjectivity)
+            return [polarity , subjectivity]
+            # print(polarity , subjectivity)
+    # def find_sentiments(self):
+    #     analysis = self.clean_tweet()
+    #     print(analysis)
+    #     # df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+    #     # df['sentiments'] = np.array([x for x in df['Tweets']])
+    #     for analysis in analysis:
+    #         # print(TextBlob(analysis).sentiment.polarity)
+    #         if TextBlob(analysis).sentiment.polarity > 0:
+    #             return 1
+    #         elif TextBlob(analysis).sentiment.polarity == 0:
+    #             return 0
+    #         else:
+    #             return -1
+        # return self.tweets_list[['polarity', 'subjectivity']]
     
     def find_lang(self) -> list:
-        return self.tweets_list["lang"]
-    
+        df = pd.DataFrame(data=[tweet.get("lang") for tweet in self.tweets], columns=['lang'])
+        # print(df)
+        return df['lang']
+            
 
     def find_favourite_count(self) -> list:
-        return self.tweets_list["favorite_count"]
-    
+        df = pd.DataFrame(data=[tweet.get("favorite_count") for tweet in self.tweets], columns=['likes'])
+        # print(df)
+        return df["likes"]    
     
     def find_retweet_count(self) -> list:
-        return self.tweets_list["retweet_count"]
+        df = pd.DataFrame(data=[tweet.get("retweet_count") for tweet in self.tweets], columns=['retweet'])
+        # print(df)
+        return df["retweet"]
     
         
     def find_screen_name(self) -> str:
-        for myList in mention:
-            for item in myList:
-                print(item.get("screen_name")  )   
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        df['user'] = np.array([tweet.get('user') for tweet in self.tweets])
+        df['screen_name'] = np.array([hash.get('screen_name') for hash in df['user']])   
+        # print(df['screen_name'])
+        return df['screen_name']
     
     def find_followers_count(self) -> list:
-        followers_count = [x.get("followers_count") for x in tweet_list["user"]]
-        for followers_count in followers_count:
-            return followers_count
-#             print(followers_count)
-            
-        
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        df['user'] = np.array([tweet.get('user') for tweet in self.tweets])
+        df['followers_count'] = np.array([hash.get('followers_count') for hash in df['user']]) 
+        # print(df['followers_count'])
+        return df['followers_count']
+                    
 
     def find_friends_count(self) -> list:
-        friends_count = [x.get("friends_count") for x in tweet_list["user"]]
-        for friends_count in friends_count:
-            return friends_count
-#             print(followers_count)
-        
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        df['user'] = np.array([tweet.get('user') for tweet in self.tweets])
+        df['friends_count'] = np.array([hash.get('friends_count') for hash in df['user']])
+        # print(df['friends_count'])
+        return df['friends_count']
+          
 
     def is_sensitive(self) -> list:
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        df['possibly_sensitive'] = np.array([tweet.get('possibly_sensitive') for tweet in self.tweets])
         try:
-            is_sensitive = self.tweets_list['possibly_sensitive']
+            is_sensitive = np.array([tweet.get('possibly_sensitive') for tweet in self.tweets])
         except KeyError:
             is_sensitive = None
-
-        return is_sensitive 
-
-    
+        # print(df['possibly_sensitive'].head(2))
+        # print(is_sensitive)
+        return is_sensitive    
 
     def find_hashtags(self) -> list:
-        hashtags = [x.get("hashtags") for x in tweet_list["entities"]]
-        return hashtags
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        df['entities'] = np.array([tweet.get('entities') for tweet in self.tweets])
+        df['hashtags'] = np.array([hash.get('hashtags') for hash in df['entities']])
+        # print(df['hashtags'].head(2))
+        return df['hashtags']
     
 
     def find_mentions(self) -> list:
-        mentions = [x.get("user_mentions") for x in tweet_list["entities"]]
-        for mention in mentions:
-            print(mention)
-#         mentions
-#         return mentions
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        df['entities'] = np.array([tweet.get('entities') for tweet in self.tweets])
+        df['mentions'] = np.array([hash.get('user_mentions') for hash in df['entities']])
+        # print(df['user_mentions'])
+        return df['mentions']
 
 
     def find_location(self) -> list:
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        df['user'] = np.array([tweet.get('user') for tweet in self.tweets])
+        df['location'] = np.array([hash.get('location') for hash in df['user']])
+        # print(df['location'].head(2))
         try:
-            location = [x.get("location") for x in self.tweets_list["user"]]
+            location = np.array([hash.get('location') for hash in df['user']])
         except TypeError:
             location = ''
         return location
@@ -144,7 +193,7 @@ class TweetDfExtractor:
         created_at = self.find_created_time()
         source = self.find_source()
         text = self.find_full_text()
-        polarity, subjectivity = self.find_sentiments(text)
+        polarity, subjectivity = self.find_sentiments()
         lang = self.find_lang()
         fav_count = self.find_favourite_count()
         retweet_count = self.find_retweet_count()
@@ -165,14 +214,53 @@ class TweetDfExtractor:
 
         return df
 
+    
+    def draw_plan(self) -> list:
+        df = pd.DataFrame(data=[tweet.get("text") for tweet in self.tweets], columns=['Tweets'])
+        df['id'] = np.array([tweet.get('id') for tweet in self.tweets])
+        df['lang'] = np.array([tweet.get('lang') for tweet in self.tweets])
+        df['likes'] = np.array([tweet.get('favorite_count') for tweet in self.tweets])
+        df['reply'] = np.array([tweet.get('retweet_count') for tweet in self.tweets])
+        df['date'] = np.array([tweet.get('created_at') for tweet in self.tweets])
+        df['user'] = np.array([tweet.get('user') for tweet in self.tweets])
+        df['entities'] = np.array([tweet.get('entities') for tweet in self.tweets])        
+        df['hashtags'] = np.array([hash.get('hashtags') for hash in df['entities']])
+        df['user'] = np.array([df])
+        df['statuses_count'] = np.array([hash.get('statuses_count') for hash in df['user']])
+        
+        # print(df['statuses_count'].head(2))
+
+        # print(df['text'])
+        return df
+
 
 if __name__ == "__main__":
+    # columns = ['created_at', 'source', 'original_text', 'polarity', 'subjectivity', 'lang', 'favorite_count',
+    #                'retweet_count',
+    #                'original_author', 'followers_count', 'friends_count', 'possibly_sensitive', 'hashtags',
+    #                'user_mentions', 'place']
     # required column to be generated you should be creative and add more features
-    _, tweet_list2 = read_json("")
-#     ./data/Economic_Twitter_Data.json
-    tweet_list = pd.DataFrame(tweet_list2).head(5)
-    print(tweet_list["id"])
-    tweet = TweetDfExtractor(tweet_list)
+    _, tweet_list = read_json("./data/Economic_Twitter_Data.json")
+    tweets = tweet_list
+    # TweetDfExtractor = TweetDfExtractor()
+    tweet = TweetDfExtractor(tweets)
+    # df = tweet.find_full_text()
+    # # df = tweet.find_statuses_count()
+    # df = tweet.find_created_time()
+    # df = tweet.find_source()
+    # df = tweet.find_lang()
+    # df = tweet.find_favourite_count()
+    # df = tweet.find_retweet_count()
+    # df = tweet.find_screen_name()
+    # df = tweet.find_followers_count()
+    # df = tweet.find_friends_count()
+    # df = tweet.is_sensitive()
+    # df = tweet.find_hashtags()
+    # df = tweet.find_mentions()
+    # df = tweet.find_location()
+    # # df = tweet.clean_tweet()
+    # df = tweet.find_sentiments()
+       
     tweet_df = tweet.get_tweet_df()
 
 
